2f8acb56c23e713347637c38ed691618db1608fe
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-10-30 12:08:38 +0600

    TypeScript learning

---
+++ output/script.js
@@ -0,0 +1,11 @@
+let country = "Bangladesh";
+country = "USA";
+let fruits = ['apple', 'mango', 'orange',];
+let marks = [4, 5, 6, 7,];
+// fruits.push('banana');
+// fruits.push(8);
+// marks.push(7);
+console.log(marks);
+console.log(fruits);
+export {};
+//# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,14 @@
+let country = "Bangladesh";
+country = "USA";
+
+let fruits: string[] = ['apple', 'mango', 'orange',]
+
+let marks: number[] = [4, 5, 6, 7,]
+
+
+// fruits.push('banana');
+// fruits.push(8);
+
+// marks.push(7);
+console.log(marks);
+console.log(fruits);
