762311126709beee6b42e6f0f0d50ed899e14bd2
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-10-30 12:12:51 +0600

    learned a new obj

--- src/script.ts
+++ src/script.ts
@@ -4,11 +4,6 @@ country = "USA";
 let fruits: string[] = ['apple', 'mango', 'orange',]
 
 let marks: number[] = [4, 5, 6, 7,]
-
-
-// fruits.push('banana');
-// fruits.push(8);
-
-// marks.push(7);
+let pair = { name: 'Mahi', marks: 50 }
 console.log(marks);
 console.log(fruits);
